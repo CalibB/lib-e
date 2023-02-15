2b16f3aeb0651138625e51c5668f0937cbd9368b
CalibB <110089911+CalibB@users.noreply.github.com>
2023-02-15 18:30:12 +0200

    feat: created empty array and Book object constructor

---
+++ index.js
@@ -0,0 +1,9 @@
+let myLibrary = [];
+
+function Book(title, author, pages, description, haveRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.description = description;
+  this.haveRead = haveRead;
+}
