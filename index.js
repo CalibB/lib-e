d31e5f127095341c426a5b1226d06e113ce0714d
CalibB <110089911+CalibB@users.noreply.github.com>
2023-02-15 19:12:08 +0200

    feat: completed functionality for getting form data into the array
    To get the data from the form, I used a submit event listener to create
    a FormData object that contains the values of the form. I then created a
    formdata event listener that takes that new form object and iterates it
    into a new array that then gets pushed into the addBookToLibrary
    function.

--- index.js
+++ index.js
@@ -9,22 +9,27 @@ function Book(title, author, pages, description, haveRead) {
 }
 
 // Get form values to be transferred into array
+
 const form = document.querySelector('#form');
 form.addEventListener('submit', (e) => {
-    // Override default behavior of a form
     e.preventDefault();
-
-    // Create a formData object to access values
     new FormData(form);
 });
 
 form.addEventListener('formdata', (e) => {
     const formData = e.formData;
 
-    // Test to see if we are getting form data
-    console.log(`${formData.get('title')}, ${formData.get('author')}, ${formData.get('pages')}, ${formData.get('description')}, ${formData.get('read')}`);
+    let bookArr = [];
+
+    for (let value of formData.values()) {
+        bookArr.push(value);
+    };
+
+    addBookToLibrary(bookArr);
+
 })
 
-function addBookToLibrary() {
-
-}
+function addBookToLibrary(formDataArray) {
+    const newBook = new Book(formDataArray[0], formDataArray[1], formDataArray[2], formDataArray[3], formDataArray[4]);
+    myLibrary.push(newBook);
+};
