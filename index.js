561cf1272b77decfafa2578ad71c515d2f870d3f
CalibB <110089911+CalibB@users.noreply.github.com>
2023-02-15 18:45:55 +0200

    feat: created basic functionality for getting form data

--- index.js
+++ index.js
@@ -7,3 +7,24 @@ function Book(title, author, pages, description, haveRead) {
   this.description = description;
   this.haveRead = haveRead;
 }
+
+// Get form values to be transferred into array
+const form = document.querySelector('#form');
+form.addEventListener('submit', (e) => {
+    // Override default behavior of a form
+    e.preventDefault();
+
+    // Create a formData object to access values
+    new FormData(form);
+});
+
+form.addEventListener('formdata', (e) => {
+    const formData = e.formData;
+
+    // Test to see if we are getting form data
+    console.log(`${formData.get('title')}, ${formData.get('author')}, ${formData.get('pages')}, ${formData.get('description')}, ${formData.get('read')}`);
+})
+
+function addBookToLibrary() {
+
+}
